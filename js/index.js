5f4417dff17b61ea48e02edacc215fc3cfd61218
Devin Udy <devin.udy@gmail.com>
2019-10-27 23:32:01 -0600

    moved mobile menu out of header

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@
 var menuBtn = document.getElementsByClassName('menu-btn');
 var mobileMenu = document.getElementsByClassName('mobile-menu');
 
-function menuActive() {
+var menuActive = function () {
     mobileMenu[0].classList.toggle('active');
 }
 
-// menuBtn[0].addEventListener('click', menuActive);
+menuBtn[0].addEventListener('click', menuActive);
